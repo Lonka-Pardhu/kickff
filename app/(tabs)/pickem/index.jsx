b1dc7944981a57d6b19213da95ecfe44ab1fb84b
Lonka Pardhu <lonkapardhu99@gmail.com>
2024-10-03 13:35:10 +0530

    separated single prediction card

--- app/(tabs)/pickem/index.jsx
+++ app/(tabs)/pickem/index.jsx
@@ -2,9 +2,6 @@ import {
   View,
   Text,
   ScrollView,
-  FlatList,
-  Image,
-  useWindowDimensions,
   Dimensions,
   TouchableOpacity,
   StyleSheet,
@@ -12,29 +9,14 @@ import {
 } from "react-native";
 import StatusBarComponent from "../../../components/customStatusBar";
 import React, { useState } from "react";
-import images from "../../../constants/images";
-import UpArrowSvg from "../../../assets/svg/UpArrowIcon";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import CustomImageCarousel from "../../../components/CustomImageCarousel";
 import UpComingSvg from "../../../assets/svg/UpComingIcon";
 import CompletedSvg from "../../../assets/svg/CompletedIcon";
-import BarcelonaSvg from "../../../assets/svg/BarcelonaIcon";
-import MadridSvg from "../../../assets/svg/MadridIcon";
-import ClockSvg from "../../../assets/svg/ClockIcon";
 import SportsCategory from "../../../components/SportsCategory";
-import LightBulbSvg from "../../../assets/svg/LightBulbIcon";
-import ArrowRightSvg from "../../../assets/svg/ArrowRightIcon";
-import CheckCircleSvg from "../../../assets/svg/CheckCircleIcon";
-import BetbuilderSvg from "../../../assets/svg/BetBuilderIcon";
-import ParalaySvg from "../../../assets/svg/ParalayIcon";
-// import YellowRoundSvg from "../../../assets/svg/YellowRoundIcon";
-import RatingComponent from "../../../components/RatingComponent";
-import { router } from "expo-router";
-import ColouredCircle from "../../../components/ColouredCircle";
-import StarSvg from "../../../assets/svg/StarIcon";
-import RoundCircleSvg from "../../../assets/svg/RoundCircleIcon";
 import BetBuilderCard from "../../../components/BetBuilderCard";
 import ParalayCard from "../../../components/ParalayCard";
+import SinglePredCard from "../../../components/SinglePredCard";
 
 const pickem = () => {
   const width = Dimensions.get("window").width;
@@ -105,85 +87,7 @@ const pickem = () => {
           <View className="px-4 py-2 flex flex-col items-center justify-center ">
             <BetBuilderCard />
             <ParalayCard />
-            <TouchableOpacity
-              onPress={() =>
-                router.push({
-                  pathname: `/pickem/singlebet`,
-                  params: { id: 1 },
-                })
-              }
-              style={styles.cardShadow}
-              className="w-full rounded-lg bg-white py-4 pr-4 pl-1 shadow-md"
-            >
-              <View className="flex flex-row items-center justify-between">
-                <View className="flex flex-row items-center">
-                  <View className="flex flex-row items-center">
-                    <View className="h-1 w-1 rounded-full bg-[#EF3E3A]"></View>
-                    <Image
-                      source={images.LeagueImg1}
-                      className="h-[40px] w-[40px]"
-                    />
-                  </View>
-                  <View className="flex flex-col">
-                    <Text className="font-sfsemibold">SerieA</Text>
-                    <View className="flex items-center flex-row gap-x-1">
-                      <Text className="font-sfsemibold text-[8px] font-medium  tracking-widest">
-                        12 MAR
-                      </Text>
-                      <View className="h-1 w-1 bg-black rounded-full"></View>
-                      <Text className="font-sfsemibold text-[8px] tracking-widest font-medium">
-                        4:30AM
-                      </Text>
-                    </View>
-                  </View>
-                </View>
-                <RatingComponent rating={4} />
-              </View>
-              <View className="pr-6 pl-10 py-2">
-                <View className="flex flex-row items-center justify-between w-full">
-                  <View className="w-3/5 flex flex-col gap-y-2 py-2">
-                    <View className="flex flex-row items-center justify-between">
-                      <View className="flex flex-row items-center gap-x-2 ">
-                        <BarcelonaSvg />
-                        <Text className="font-sfsemibold">Barcelona</Text>
-                      </View>
-                      {/* <Text className="font-sfsemibold">2</Text> */}
-                    </View>
-                    <View className="flex flex-row items-center justify-between">
-                      <View className="flex flex-row items-center gap-x-2 ">
-                        <MadridSvg />
-                        <Text className="font-sfsemibold text-[#979797]">
-                          Real Madrid
-                        </Text>
-                      </View>
-                      {/* <Text className="font-sfsemibold text-[#979797]">0</Text> */}
-                    </View>
-                  </View>
-                  <View className="flex flex-row items-center justify-center gap-x-1 ">
-                    <ClockSvg />
-                    <Text className="font-sfregular font-medium text-[#FFAB2E]">
-                      2.54
-                    </Text>
-                  </View>
-                </View>
-              </View>
-              <View className="px-4 w-full flex flex-row items-center justify-between">
-                <View className="flex flex-row items-center gap-x-2 w-[85%]">
-                  <LightBulbSvg />
-                  <Text
-                    numberOfLines={2}
-                    className="font-sfregular font-medium text-[#838E94] text-[12px]"
-                  >
-                    Lorem Ipsum Is Simply Dummy Text Of The Printing And
-                    Typesetting Industry. Lorem Ipsum Has Been The Industry’s
-                    Standard Dummy Text Ever Since The 1500S, When An Unknown
-                    Printer Took A Galley Of Type And Scrambled It To Make A
-                    Type Specimen Book.
-                  </Text>
-                </View>
-                <ArrowRightSvg />
-              </View>
-            </TouchableOpacity>
+            <SinglePredCard />
           </View>
         </ScrollView>
       </StatusBarComponent>
--- components/SinglePredCard.jsx
+++ components/SinglePredCard.jsx
@@ -1,12 +1,104 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 import React from "react";
+import images from "../constants/images";
+import RatingComponent from "./RatingComponent";
+import MadridSvg from "../assets/svg/MadridIcon";
+import ClockSvg from "../assets/svg/ClockIcon";
+import LightBulbSvg from "../assets/svg/LightBulbIcon";
+import ArrowRightSvg from "../assets/svg/ArrowRightIcon";
+import { TouchableOpacity } from "react-native";
+import { useRouter } from "expo-router";
+import BarcelonaSvg from "../assets/svg/BarcelonaIcon";
 
-const SingleBetCard = () => {
+const SinglePredCard = () => {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>SingleBetCard</Text>
-    </View>
+    <TouchableOpacity
+      onPress={() =>
+        router.push({
+          pathname: `/pickem/singlebet`,
+          params: { id: 1 },
+        })
+      }
+      style={styles.cardShadow}
+      className="w-full rounded-lg bg-white py-4 pr-4 pl-1 shadow-md md-2"
+    >
+      <View className="flex flex-row items-center justify-between">
+        <View className="flex flex-row items-center">
+          <View className="flex flex-row items-center">
+            <View className="h-1 w-1 rounded-full bg-[#EF3E3A]"></View>
+            <Image source={images.LeagueImg1} className="h-[40px] w-[40px]" />
+          </View>
+          <View className="flex flex-col">
+            <Text className="font-sfsemibold">SerieA</Text>
+            <View className="flex items-center flex-row gap-x-1">
+              <Text className="font-sfsemibold text-[8px] font-medium  tracking-widest">
+                12 MAR
+              </Text>
+              <View className="h-1 w-1 bg-black rounded-full"></View>
+              <Text className="font-sfsemibold text-[8px] tracking-widest font-medium">
+                4:30AM
+              </Text>
+            </View>
+          </View>
+        </View>
+        <RatingComponent rating={4} />
+      </View>
+      <View className="pr-6 pl-10 py-2">
+        <View className="flex flex-row items-center justify-between w-full">
+          <View className="w-3/5 flex flex-col gap-y-2 py-2">
+            <View className="flex flex-row items-center justify-between">
+              <View className="flex flex-row items-center gap-x-2 ">
+                <BarcelonaSvg />
+                <Text className="font-sfsemibold">Barcelona</Text>
+              </View>
+              {/* <Text className="font-sfsemibold">2</Text> */}
+            </View>
+            <View className="flex flex-row items-center justify-between">
+              <View className="flex flex-row items-center gap-x-2 ">
+                <MadridSvg />
+                <Text className="font-sfsemibold text-[#979797]">
+                  Real Madrid
+                </Text>
+              </View>
+              {/* <Text className="font-sfsemibold text-[#979797]">0</Text> */}
+            </View>
+          </View>
+          <View className="flex flex-row items-center justify-center gap-x-1 ">
+            <ClockSvg />
+            <Text className="font-sfregular font-medium text-[#FFAB2E]">
+              2.54
+            </Text>
+          </View>
+        </View>
+      </View>
+      <View className="px-4 w-full flex flex-row items-center justify-between">
+        <View className="flex flex-row items-center gap-x-2 w-[85%]">
+          <LightBulbSvg />
+          <Text
+            numberOfLines={2}
+            className="font-sfregular font-medium text-[#838E94] text-[12px]"
+          >
+            Lorem Ipsum Is Simply Dummy Text Of The Printing And Typesetting
+            Industry. Lorem Ipsum Has Been The Industry’s Standard Dummy Text
+            Ever Since The 1500S, When An Unknown Printer Took A Galley Of Type
+            And Scrambled It To Make A Type Specimen Book.
+          </Text>
+        </View>
+        <ArrowRightSvg />
+      </View>
+    </TouchableOpacity>
   );
 };
 
-export default SingleBetCard;
+export default SinglePredCard;
+
+const styles = StyleSheet.create({
+  cardShadow: {
+    shadowOffset: { width: -2, height: 4 },
+    shadowColor: "#171717",
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+});
